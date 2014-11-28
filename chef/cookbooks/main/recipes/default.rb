48273d15b24bd36b5fa84bd6c10a2b78b2c9244a
David <davidhuangdw@163.com>
2014-11-28 20:04:15 +0800

    templates

--- chef/cookbooks/main/recipes/default.rb
+++ chef/cookbooks/main/recipes/default.rb
@@ -1,1 +1,15 @@
 package 'git-core'
+package 'zsh'
+
+user node[:user][:name] do
+  password node[:user][:password]
+  gid "adm"
+  home "/home/#{node[:user][:name]}"
+  supports manage_home: true
+  shell "/usr/bin/zsh"
+end
+
+template "/home/#{node[:user][:name]}/.zshrc" do
+  source "zshrc.erb"
+  owner node[:user][:name]
+end
